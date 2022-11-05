d46150024ea425109dd09596a66b4298296c63a3
Yunus Gungor <yunusgnr@gmail.com>
2022-11-05 12:22:17 +0100

    formating and implemented decoding

--- tests/test_packet.py
+++ tests/test_packet.py
@@ -2,37 +2,71 @@
 import pytest
 from custom_components.blaueberg.packet import *
 
-    
+value_overflow_error = OverflowError("value does not fit into the byte size")
+
+
 @pytest.mark.parametrize(
-    "value,byte_size,exception", [(0, -1, ValueError("byte_size can not be less than zero")), (0x111, 1, OverflowError("value does not fit into the byte size"))]
+    "value,byte_size,exception", [
+        (0, -1, ValueError("byte_size can not be less than zero")), (0x111, 1, value_overflow_error)]
 )
-def test_section_init_with_exception(value:int,byte_size:int,exception:Exception):
+def test_section_init_with_exception(value: int, byte_size: int, exception: Exception):
     with pytest.raises(type(exception)) as exc_info:
         Section(value, byte_size)
     assert str(exception) == str(exc_info.value)
 
+
 @pytest.mark.parametrize(
-    "value,byte_size", [(0, 0), (0, 1), (0x11, 1), (0x1FFF,2)]
+    "value,byte_size", [(0, 0), (0, 1), (0x11, 1), (0xFF, 1), (0x1FFF, 2)]
 )
-def test_section_init(value:int,byte_size:int):
+def test_section_init(value: int, byte_size: int):
     Section(value, byte_size)
 
-    
+
 @pytest.mark.parametrize(
-    "value,byte_size,expected", [(0, 0, bytes([0])), (0, 1, bytes([0])), (0x11, 1, bytes([0x11])), (0x1FFF,2, bytes([0x1F,0xFF])), (0x1FFF,3, bytes([0x0, 0x1F,0xFF]))]
+    "new_value,byte_size,expected", [(bytes([0xFF]), 1, 0xFF), (bytes([0xFF, 0x11]), 3, 0xFF11), (bytes([
+        0x0, 0x0, 0x11]), 3, 0x11), (bytes([0x0, 0x0, 0x0, 0x11]), 3, 0x11)]
 )
-def test_section_to_bytes(value:int,byte_size:int, expected:bytes):
+def test_section_set_value(new_value: bytes, byte_size: int, expected: int):
+    assert Section(0, byte_size).set_value(new_value).value == expected
+
+
+@pytest.mark.parametrize(
+    "new_value,byte_size,exception", [
+        (bytes([0xFF, 0x11]), 1, value_overflow_error)]
+)
+def test_section_set_value_with_exception(new_value: bytes, byte_size: int, exception: Exception):
+    with pytest.raises(type(exception)) as exc_info:
+        Section(0, byte_size).set_value(new_value)
+    assert str(exception) == str(exc_info.value)
+
+
+@pytest.mark.parametrize(
+    "value,byte_size,expected", [(0, 0, bytes([0])), (0, 1, bytes([0])), (0x11, 1, bytes(
+        [0x11])), (0x1FFF, 2, bytes([0x1F, 0xFF])), (0x1FFF, 3, bytes([0x0, 0x1F, 0xFF]))]
+)
+def test_section_to_bytes(value: int, byte_size: int, expected: bytes):
     assert Section(value, byte_size).to_bytes() == expected
 
+
 @pytest.mark.parametrize(
-    "sections,expected", [([Section(0x1,2),Section(0xF)],0x010F),([Section(0x1),Section(0x1FFF,2)],0x011FFF)]
+    "sections,expected", [([Section(0x1, 2), Section(0xF)], 0x010F), ([
+        Section(0x1), Section(0x1FFF, 2)], 0x011FFF)]
 )
-def test_packet_to_int(sections:List[Section], expected:int):
+def test_packet_to_int(sections: List[Section], expected: int):
     assert Packet(sections).to_int() == expected
 
-    
+
 @pytest.mark.parametrize(
-    "sections,expected", [([Section(0x1,2),Section(0xF)],bytes([0x0,0x01,0x0F])),([Section(0x1),Section(0x1FFF,2)],bytes([0x01,0x1F,0xFF]))]
+    "sections,expected", [([Section(0x1, 2), Section(0xF)], bytes([0x0, 0x01, 0x0F])), ([
+        Section(0x1), Section(0x1FFF, 2)], bytes([0x01, 0x1F, 0xFF]))]
 )
-def test_packet_to_bytes(sections:List[Section], expected:int):
+def test_packet_to_bytes(sections: List[Section], expected: int):
     assert Packet(sections).to_bytes() == expected
+
+
+@pytest.mark.parametrize(
+    "sections,value,expected", [([Section(0,2), Section(0,1)], bytes(
+        [0x0, 0x01, 0x0F]), [Section(0x1, 2), Section(0xF, 1)])]
+)
+def test_packet_decode(sections: List[Section], value: bytes, expected: List[Section]):
+    assert Packet(sections).decode(value) == expected
