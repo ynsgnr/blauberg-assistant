abb0a9e1a37f82562042da0233372796a83999c0
Yunus Gungor <yunusgnr@gmail.com>
2022-11-05 11:03:58 +0100

    migrated to built in function bit_length instead of minDigit

--- custom_components/blaueberg/packet.py
+++ custom_components/blaueberg/packet.py
@@ -1,31 +1,30 @@
 from typing import List
-from math import floor, log, ceil
+from math import ceil
 
 byte_size_error = ValueError("byte_size can not be less than zero")
-value_fit_error = ValueError("value does not fit into the byte size")
+value_fit_error = OverflowError("value does not fit into the byte size")
 
 class Section:
     """Represents a section of a packet"""
 
     __slots__ = ['value', 'byte_size']
-    
-    def _minDigit(self, n:int, base:int=16) -> int:
-        if n == 0:
-            return 1
-        if n == 1:
-            return 1
-        return floor(log(abs(n))/log(base)+1)
 
     def __init__(self,  value:int = 0, byte_size:int = 0):
-        minumum_byte_size = ceil(self._minDigit(value)/2)
+        value_size = ceil(value.bit_length()/8)
+        # (0).bit_length is zero but we need byte size at least 1
+        if value_size == 0:
+            value_size = 1
         if byte_size==0:
-            byte_size = minumum_byte_size
+            byte_size = value_size
         if byte_size<0:
             raise byte_size_error
-        if minumum_byte_size>byte_size:
+        if value_size>byte_size:
             raise value_fit_error
         self.value = value
         self.byte_size = byte_size
+
+    def to_bytes(self) -> bytes:
+        return self.value.to_bytes(self.byte_size, byteorder='big')
     
     def __str__(self) -> str:
         # each byte has two chars in hex reporesantation
@@ -37,7 +36,7 @@ class Section:
 class Packet(List[Section]):
     """Represents a packet as a list of sections, it extends on list itself so all methods for lists can be used"""
 
-    def build(self) -> int:
+    def to_int(self) -> int:
         packet:int = 0
         for section in self:
             packet = packet << section.byte_size*8 | section.value
@@ -48,4 +47,4 @@ class Packet(List[Section]):
     
     def __str__(self) -> str:
         # each byte has two chars in hex reporesantation
-        return "0x{0:0{1}X}".format(self.build(),self.byte_size()*2)
+        return "0x{0:0{1}X}".format(self.to_int(),self.byte_size()*2)
--- tests/test_packet.py
+++ tests/test_packet.py
@@ -2,22 +2,9 @@
 import pytest
 from custom_components.blaueberg.packet import *
 
-@pytest.mark.parametrize(
-    "n,expected", [(0x0, 1), (0x1, 1), (0x2, 1), (0x10, 2), (0x11, 2), (0xFF, 2), (0x00FF, 2)]
-)
-def test_section_minDigit_hex(n:int, expected:int):
-    assert Section()._minDigit(n) == expected # type: ignore
-
     
 @pytest.mark.parametrize(
-    "n,expected", [(0, 1), (1, 1), (2, 1), (10, 2), (11, 2), (1111, 4)]
-)
-def test_section_minDigit_decimal(n:int, expected:int):
-    assert Section()._minDigit(n,10) == expected # type: ignore
-
-    
-@pytest.mark.parametrize(
-    "value,byte_size,exception", [(0, -1, ValueError("byte_size can not be less than zero")), (0x111, 1, ValueError("value does not fit into the byte size"))]
+    "value,byte_size,exception", [(0, -1, ValueError("byte_size can not be less than zero")), (0x111, 1, OverflowError("value does not fit into the byte size"))]
 )
 def test_section_init_with_exception(value:int,byte_size:int,exception:Exception):
     with pytest.raises(type(exception)) as exc_info:
@@ -30,8 +17,15 @@ def test_section_init_with_exception(value:int,byte_size:int,exception:Exception
 def test_section_init(value:int,byte_size:int):
     Section(value, byte_size)
 
+    
 @pytest.mark.parametrize(
-    "sections,expected", [([Section(0x1,2),Section(0xF,1)],0x010F),([Section(0x1,1),Section(0x1FFF,2)],0x011FFF)]
+    "value,byte_size,expected", [(0, 0, bytes([0])), (0, 1, bytes([0])), (0x11, 1, bytes([0x11])), (0x1FFF,2, bytes([0x1F,0xFF])), (0x1FFF,3, bytes([0x0, 0x1F,0xFF]))]
+)
+def test_section_to_bytes(value:int,byte_size:int, expected:bytes):
+    assert Section(value, byte_size).to_bytes() == expected
+
+@pytest.mark.parametrize(
+    "sections,expected", [([Section(0x1,2),Section(0xF)],0x010F),([Section(0x1),Section(0x1FFF,2)],0x011FFF)]
 )
 def test_packet_build(sections:List[Section], expected:int):
-    assert Packet(sections).build() == expected
+    assert Packet(sections).to_int() == expected
