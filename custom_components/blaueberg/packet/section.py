89fc5e4ab586a9cde539ab38bd5dc4acd247df8d
Yunus Gungor <yunusgnr@gmail.com>
2022-11-19 12:24:00 +0100

    trailing or leading zeros in section

--- custom_components/blaueberg/packet/section.py
+++ custom_components/blaueberg/packet/section.py
@@ -1,16 +1,17 @@
 from __future__ import annotations
 from typing import Any
 from math import ceil
+from .zeros import Zeros
 from .errors import byte_size_error,value_overflow_error
 
 class Section:
     """Represents a section of a packet"""
 
-    __slots__ = ['value', 'byte_size']
+    __slots__ = ['value', 'byte_size', 'trail_or_lead']
 
     @staticmethod
-    def Template(byte_size: int) -> Section:
-        return Section(0, byte_size)
+    def Template(byte_size: int, trail_or_lead: Zeros = Zeros.LEADING) -> Section:
+        return Section(0, byte_size, trail_or_lead)
 
     @staticmethod
     def _minimum_byte_size(value: int) -> int:
@@ -19,7 +20,7 @@ class Section:
             return 1
         return size
 
-    def __init__(self,  value: int = 0, byte_size: int = 0):
+    def __init__(self,  value: int = 0, byte_size: int = 0, trail_or_lead: Zeros = Zeros.LEADING):
         value_size = self._minimum_byte_size(value)
         if byte_size == 0:
             byte_size = value_size
@@ -29,12 +30,14 @@ class Section:
             raise value_overflow_error
         self.value = value
         self.byte_size = byte_size
+        self.trail_or_lead = trail_or_lead
 
     def to_bytes(self) -> bytes:
-        return self.value.to_bytes(self.byte_size, byteorder='big')
+        return Zeros.insert(self.trail_or_lead, self.value.to_bytes(self.byte_size, byteorder='big'), self.byte_size)
 
     def set_bytes(self, bytes_value: bytes) -> Section:
-        new_value = int.from_bytes(bytes_value, "big")
+        value = Zeros.insert(self.trail_or_lead, bytes_value, self.byte_size)
+        new_value = int.from_bytes(value, "big")
         new_value_size = self._minimum_byte_size(new_value)
         if new_value_size > self.byte_size:
             raise value_overflow_error
--- tests/packet/test_section.py
+++ tests/packet/test_section.py
@@ -23,11 +23,17 @@ def test_section_init(value: int, byte_size: int):
 
 @pytest.mark.parametrize(
     "new_value,byte_size,expected", [(bytes([0xFF]), 1, 0xFF), (bytes([0xFF, 0x11]), 3, 0xFF11), (bytes([
-        0x0, 0x0, 0x11]), 3, 0x11), (bytes([0x0, 0x0, 0x0, 0x11]), 3, 0x11)]
+        0x0, 0x0, 0x11]), 3, 0x11)]
 )
 def test_section_set_bytes(new_value: bytes, byte_size: int, expected: int):
     assert Section(0, byte_size).set_bytes(new_value).value == expected
 
+@pytest.mark.parametrize(
+    "new_value,byte_size,expected", [(bytes([0xFF]), 1, 0xFF), (bytes([0xFF, 0x11]), 3, 0xFF1100), (bytes([
+        0x0, 0x0, 0x11]), 3, 0x11)]
+)
+def test_section_set_bytes_with_trailing_zeros(new_value: bytes, byte_size: int, expected: int):
+    assert Section(0, byte_size, trail_or_lead=Zeros.TRAILING).set_bytes(new_value).value == expected
 
 @pytest.mark.parametrize(
     "new_value,byte_size,exception", [
